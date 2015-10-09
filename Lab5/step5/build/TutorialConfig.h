f00f52acf0143c9420b4d8e8a161a894a32d267a
ianhuntress <ihuntress27@gmail.com>
2015-10-09 17:51:50 +0000

    lab5

---
+++ Lab5/step5/build/TutorialConfig.h
@@ -0,0 +1,8 @@
+
+
+#define Tutorial_VERSION_MAJOR 1
+#define Tutorial_VERSION_MINOR 0
+#define USE_MYMATH
+// the configured options and settings for Tutorial
+/* #undef HAVE_LOG */
+/* #undef HAVE_EXP */
